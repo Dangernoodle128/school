fc33945bc65a9cb08120c7e1788cd52a5aab4330
ArockyaJ <arockyajoseph@gmail.com>
2023-05-04 17:44:47 +0530

    de

--- src/app/core/Components/dashboardd/dashboardd.component.ts
+++ src/app/core/Components/dashboardd/dashboardd.component.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 
+interface Comment {
+  content: string,
+  reply?: Comment[]
+}
+
 @Component({
   selector: 'app-dashboardd',
   templateUrl: './dashboardd.component.html',
   styleUrls: ['./dashboardd.component.scss']
 })
+
 export class DashboarddComponent {
+  value !: string;
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
@@ -30,4 +37,27 @@ export class DashboarddComponent {
   );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
+
+  comm!: string;
+  rep : string[] = [];
+  show = false;
+  currentDateTime: Date = new Date();
+  results: Comment[] = [];
+
+  onSubmit() {
+    this.results.push({content: this.comm, reply: []})
+    this.comm = " ";
+  }
+  onReplay(i: number) {
+    this.results[i].reply?.push({content: this.rep[i] });
+    this.rep[i] = " ";
+    this.show= false;
+  }
+  Onshow(){
+      if(this.show == false){
+        this.show = true;
+      }else{
+        this.show = false;
+      }
+     }
 }
