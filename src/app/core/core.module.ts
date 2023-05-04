50d42dfc6dc70b65dc40ba5797b2eb2f61669932
EsmondTony <esmondtony543@gmail.com>
2023-05-04 17:45:50 +0530

    corousel

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { MaterialModule } from '../material/material.module';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { CarouselComponent } from './Components/carousel/carousel.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    DashboardComponent
+    DashboardComponent,
+    CarouselComponent
   ],
   imports: [
     CommonModule,
